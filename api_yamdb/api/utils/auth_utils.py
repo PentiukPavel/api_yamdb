de8fa96aa69b5b106a0773907e5d8928fbbf5177
valexandro <shark1501@gmail.com>
2022-10-21 12:54:42 +1000

    Переработка отправки писем с кодом

--- api_yamdb/api/utils/auth_utils.py
+++ api_yamdb/api/utils/auth_utils.py
@@ -1,11 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
 
+User = get_user_model()
 
-def send_confirmation_code(user_email, confirmation_code):
+
+def send_confirmation_code(user: User, confirmation_code: str):
     """Отправляет почту через выбранный email backend."""
     send_mail(
-        subject='Код подтверждения для получения токена',
+        subject=f'Код подтверждения Yamdb для пользователя {user.username}',
         message=confirmation_code,
-        from_email='yamdb@test.com',
-        recipient_list=[user_email, ],
+        from_email=u'yamdb@test.com',
+        recipient_list=[user.email, ],
         fail_silently=False,)
