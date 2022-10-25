020f88283a2b9e0218be9c2d50b59a2efac27d12
Амир Нурмухамбетов <thereug6@gmail.com>
2022-10-25 13:28:22 +0600

    Fixed unecessary read only title

--- api_yamdb/api/v1/serializers.py
+++ api_yamdb/api/v1/serializers.py
@@ -145,4 +145,3 @@ class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = ('id', 'text', 'author', 'score', 'pub_date',)
-        read_only_fields = ('title',)
